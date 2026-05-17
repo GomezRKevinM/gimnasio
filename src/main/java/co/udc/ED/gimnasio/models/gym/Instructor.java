35d657ff27b48abb87ed7df80fdbefa2a73749b4
kevin-desktop <kgomezr2@unicartagena.edu.co>
2026-05-17 12:58:40 -0500

    feat: creacion del record Instructor - Kevin Gómez

--- src/main/java/co/udc/ED/gimnasio/models/gym/Instructor.java
+++ src/main/java/co/udc/ED/gimnasio/models/gym/Instructor.java
@@ -1,8 +1,36 @@
 package co.udc.ED.gimnasio.models.gym;
 
+import java.util.UUID;
+
 /**
  * @Author: Kevinn Gómez
  * @Description: Record empleado para modelar al instructor del gimnasio
  */
-public record Instructor() {
+public record Instructor(String nombre, UUID codigo) {
+
+    @Override
+    public boolean equals(Object obj) {
+        if(this == obj ) return true;
+        if(obj == null) return false;
+
+        // Permite buscar por texto o por objeto
+        if(obj instanceof String){
+            return this.codigo.equals(UUID.fromString((String) obj));
+        }
+        if(obj instanceof Instructor){
+            Instructor otro = (Instructor) obj;
+            return this.codigo.equals(otro.codigo);
+        }
+        return false;
+    }
+
+    @Override
+    public String toString(){
+        return String.format("[ Nombre: %s | Código: %s ]", this.nombre, this.codigo);
+    }
+
+    @Override
+    public int hashCode() {
+        return this.codigo.hashCode();
+    }
 }
