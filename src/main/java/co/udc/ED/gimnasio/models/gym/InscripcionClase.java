8368f28c70a668cf8dfa00534ca6f21f35134f26
kevin-desktop <kgomezr2@unicartagena.edu.co>
2026-05-17 14:10:18 -0500

    feat: creacion del record para la inscripcion de clase - Kevin Gómez

--- src/main/java/co/udc/ED/gimnasio/models/gym/InscripcionClase.java
+++ src/main/java/co/udc/ED/gimnasio/models/gym/InscripcionClase.java
@@ -1,8 +1,28 @@
 package co.udc.ED.gimnasio.models.gym;
 
+import java.sql.Timestamp;
+
 /**
  * @Author: Kevin Gómez
  * @Description: Record usado para modelar datos de una inscripción a una clase
  */
-public record InscripcionClase() {
+public record InscripcionClase(Timestamp fechaInscripcion, String codigoClase, String cliente) {
+
+    @Override
+    public boolean equals(Object obj){
+        if(obj instanceof InscripcionClase other){
+            return this.codigoClase.equals(other.codigoClase) && this.cliente.equals(other.cliente);
+        }
+        return false;
+    }
+
+    @Override
+    public String toString(){
+        return String.format("[ Fecha: %s | Cliente: %s | Código de Clase: %s ]",  fechaInscripcion, cliente, codigoClase);
+    }
+
+    @Override
+    public int hashCode(){
+        return (codigoClase + cliente).hashCode();
+    }
 }
